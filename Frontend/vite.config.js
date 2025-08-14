c8ef26f91d16b3c2f02367d10bf8560b9ccf706b
Bikash-design-lab <vmodi5425@gmail.com>
2025-08-14 15:09:10 +0530

    fix:error

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    cssMinify: true,
+    cssCodeSplit: true,
+    rollupOptions: {
+      output: {
+        manualChunks: undefined
+      }
+    }
+  },
+  css: {
+    postcss: './postcss.config.js'
+  }
 })
